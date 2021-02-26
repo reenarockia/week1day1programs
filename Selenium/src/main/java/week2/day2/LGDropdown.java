972fd8a802ef85e1a081088c78209c36a497e87c
johna <johna@DESKTOP-H4SD3F7>
2021-02-26 22:06:52 +0530

    260221

--- Selenium/src/main/java/week2/day2/LGDropdown.java
+++ Selenium/src/main/java/week2/day2/LGDropdown.java
@@ -16,20 +16,37 @@ public class LGDropdown {
 		ChromeDriver driver=new ChromeDriver();
 		driver.get("http://leafground.com/pages/Dropdown.html");
 		driver.manage().window().maximize();
-		Select dropDown1=new Select(driver.findElement(By.id("dropdown1")));
+		
+		//select training program by index
+		WebElement dropDown1Element=driver.findElement(By.id("dropdown1"));
+		Select dropDown1=new Select(dropDown1Element);
 		dropDown1.selectByIndex(1);
+	
+//		if(dropDown1Element.getAttribute("value")== "Selenium")
+//		System.out.println("The selected value in dropdown1: Selenium - Test Pass");
+//		else
+//			System.out.println("Test Fail");
+//		
+		//Select training program using text
 		Select dropDown2=new Select(driver.findElement(By.name("dropdown2")));
 		dropDown2.selectByVisibleText("Appium");
+		
+		//select training program using value
 		Select dropDown3=new Select(driver.findElement(By.id("dropdown3")));
 		dropDown3.selectByValue("3");
-		List<WebElement> dropdownlist=driver.findElements(By.xpath("//select"));
+		
+		//get the number of dropdown options
+		List<WebElement> dropdownlist=driver.findElements(By.tagName("select"));
 		System.out.println("Number of dropdowns present: "+dropdownlist.size());
+		
+		//Use sendKeys to select
 		driver.findElement(By.xpath("//option[contains(text(),'sendKeys')]/parent::select")).sendKeys("Loadrunner");
+		
+		//Select multiple choices
 		Select dropDown5=new Select(driver.findElement(By.xpath("//option[contains(text(),'programs')]/parent::select[@multiple]")));
 		dropDown5.selectByVisibleText("Selenium");
 		dropDown5.selectByVisibleText("UFT/QTP");
 		//driver.close();
-		
 	}
 
 }
