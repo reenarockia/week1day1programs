1d4d9d5df3a4ae507bec8c583e8dcec8641c4e99
johna <johna@DESKTOP-H4SD3F7>
2021-02-26 15:05:21 +0530

    changes on 260221

--- Selenium/src/main/java/week2/day2/LGRadioButtonPageTest.java
+++ Selenium/src/main/java/week2/day2/LGRadioButtonPageTest.java
@@ -1,7 +1,9 @@
 package week2.day2;
 
+import java.util.List;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class LGRadioButtonPageTest {
@@ -12,6 +14,21 @@ public class LGRadioButtonPageTest {
 		driver.get("http://leafground.com/pages/radio.html");
 		driver.manage().window().maximize();
 		
+		//to click on yes radio button for "Are you enjoying the classes?"
+		driver.findElement(By.id("yes")).click();
+		
+		//To find the default selected button- result - checked
+		WebElement defaultCheck=driver.findElement(By.xpath("//label[@for='Checked']"));
+		System.out.println("default selected radio button name is: "+ defaultCheck.getText());
+		
+		//To select the age group (only if the choice is not selected)
+		List<WebElement> ageGroup=driver.findElements(By.name("age"));
+		
+		for ( WebElement webElement : ageGroup) {
+			if(!webElement.isSelected()) {
+				webElement.click();
+				}
+		}
 	}
 
 }
