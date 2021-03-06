194a0ab14777fe15a2d854f9add9bd042c7b7285
johna <johna@DESKTOP-H4SD3F7>
2021-03-06 22:53:24 +0530

    060321 week3 day1 Assignment 1 jQuery

---
+++ Selenium/src/main/java/week4/day1/JquesryUITest.java
@@ -0,0 +1,50 @@
+package week4.day1;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class JquesryUITest {
+
+	public static void main(String[] args) throws InterruptedException, IOException {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("https://jqueryui.com/sortable/");
+		driver.manage().window().maximize();
+		Thread.sleep(3000);
+		
+		driver.switchTo().frame(0);
+		
+		WebElement item1=driver.findElement(By.xpath("//li[text()='Item 1']"));
+		WebElement item4=driver.findElement(By.xpath("//li[text()='Item 4']"));
+       
+        int targetLocationX=item4.getLocation().getX();
+		int targetLocationY=item4.getLocation().getY();
+		 JavascriptExecutor js = (JavascriptExecutor) driver;
+		 js.executeScript("arguments[0].scrollIntoView(true);", item1);
+		
+		Actions actionsObj=new Actions(driver);
+		actionsObj.dragAndDropBy(item1, targetLocationX,targetLocationY).perform();
+		
+		File source=driver.getScreenshotAs(OutputType.FILE);
+		File target=new File("./snaps/JqueryItem1Relocate.png");
+		FileUtils.copyFile(source, target);
+		System.out.println("The screenshot is in snaps");
+		
+		System.out.println("Item 1 is relocated to Item 4");
+		driver.switchTo().defaultContent();
+		driver.close();
+
+		
+	}
+
+}
